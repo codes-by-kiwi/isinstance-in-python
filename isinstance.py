7e177d94ec4fb33527f1191544cd07868b81c0d6
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-27 11:39:03 +0100

    Add files via upload

---
+++ isinstance.py
@@ -0,0 +1,7 @@
+#The isinstance() function returns True if the specified object is of the specified type, otherwise False.
+
+d = isinstance(6, float)
+print(d)
+
+
+#Since 6 is not a float the value of d becomes "False"
